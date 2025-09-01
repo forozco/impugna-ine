e7acd98f6f1688f33bb66765d6ed6ddb2f30a866
Fernando Orozco <f_o15@hotmail.com>
2025-09-01 15:38:41 -0600

    Ajustes
    Ajustes

--- src/app/shared/layout/layout/layout.component.ts
+++ src/app/shared/layout/layout/layout.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from '../header/header.component';
-import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
 })
 export class LayoutComponent {}
